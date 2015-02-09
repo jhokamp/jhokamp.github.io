aab8a9aca9a30d2005f31490174311dbc3e7fc38
Naveen Shaji <naveen@pixelblenders.com>
2015-02-10 02:49:39 +0530

    Finished up basic layout. Added...
    Added a new blog post. Added clamp after 5 lines on cards. Fixed footer.
    Made into sticky footer. Added content. Now need to change scrollbars to
    something appealing yet light.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function () {
     $(".button-collapse").sideNav();
 })
+$(document).ready(function() {
+$(window).resize(function(){
+  var bodyHeight = $("body").height();
+  var vwptHeight = $(window).height();
+  if (vwptHeight > bodyHeight) {
+    $("footer").css("position","absolute").css("bottom",0);
+  }
+});
+    $(window).resize();
+});
